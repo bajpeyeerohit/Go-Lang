a97456c3d63ee68e12bfaa99f58ed04869fd29bb
bajpeyeerohit <rohit760583@gmail.com>
2025-06-28 09:17:02 +0530

    change

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 
 	fmt.Print("heloo")
+
+   app:=fiber.New()
+   app.Get("/abc",func(c*fiber.Ctx)error{
+      return c.SendString("hello world!");
+   })
+   app.Listen(":3000")
 }
